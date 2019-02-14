5032d8df00b6bc846d36d86f5194ef30709df4db
EMILY <ley03300@gmail.com>
2019-02-14 15:45:21 -0800

    test

--- test.py
+++ test.py
@@ -40,8 +40,8 @@ if __name__ == '__main__':
                 scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
                 v = raw * scale
                 temp = (v - 500) / 10 - 6
-
                 sleep(1)
+                print(t)
 
 
 
