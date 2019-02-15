a313ffe0ac1ca10d8a197c81c5682a214ed2ccf7
EMILY <ley03300@gmail.com>
2019-02-14 17:54:29 -0800

    test

--- gossip_bt_server.py
+++ gossip_bt_server.py
@@ -1,81 +1,3 @@
-'''from btserver import BTServer
-from bterror import BTError
-from neo import Gpio  # import Gpio library
-from time import sleep
-
-
-import argparse
-import asyncore
-import json
-from random import uniform
-from threading import Thread
-from time import sleep, time
-
-if __name__ == '__main__':
-    # Create option parser
-    usage = "usage: %prog [options] arg"
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--output", dest="output_format", default="csv", help="set output format: csv, json")
-
-    args = parser.parse_args()
-
-    # Create a BT server
-    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-    service_name = "GossipBTServer"
-    server = BTServer(uuid, service_name)
-
-    # Create the server thread and run it
-    server_thread = Thread(target=asyncore.loop, name="Gossip BT Server Thread")
-    server_thread.daemon = True
-    server_thread.start()
-
-    while True:
-        for client_handler in server.active_client_handlers.copy():
-            # Use a copy() to get the copy of the set, avoiding 'set change size during iteration' error
-            # Create CSV message "'realtime', time, temp, SN1, SN2, SN3, SN4, PM25\n"
-
-
-            while True:
-                raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-                scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
-                v = raw * scale
-                temp = (v - 500) / 10 - 6
-
-                sleep(1)
-
-
-
-            epoch_time = int(time())    # epoch time
-
-            SN1 = uniform(40, 50)       # random SN1 value
-            SN2 = uniform(60, 70)       # random SN2 value
-            SN3 = uniform(80, 90)       # random SN3 value
-            SN4 = uniform(100, 110)     # random SN4 value
-            PM25 = uniform(120, 130)    # random PM25 value
-
-            msg = ""
-            if args.output_format == "csv":
-                msg = "realtime, %d, %f, %f, %f, %f, %f, %f" % (epoch_time, temp, SN1, SN2, SN3, SN4, PM25)
-            elif args.output_format == "json":
-                output = {'type': 'realtime',
-                          'time': epoch_time,
-                          'temp': temp,
-                          'SN1': SN1,
-                          'SN2': SN2,
-                          'SN3': SN3,
-                          'SN4': SN4,
-                          'PM25': PM25}
-                msg = json.dumps(output)
-            try:
-                client_handler.send(msg + '\n')
-            except Exception as e:
-                BTError.print_error(handler=client_handler, error=BTError.ERR_WRITE, error_message=repr(e))
-                client_handler.handle_close()
-
-            # Sleep for 3 seconds
-        sleep(3)
-'''
-
 from btserver import BTServer
 from bterror import BTError
 from neo import Gpio  # import Gpio library
@@ -118,8 +40,8 @@ if __name__ == '__main__':
                 scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
                 v = raw * scale
                 temp = (v - 500) / 10 - 6
+
                 sleep(1)
-                print(t)
 
 
 
@@ -152,6 +74,7 @@ if __name__ == '__main__':
 
             # Sleep for 3 seconds
         sleep(3)
+
 
 
 
