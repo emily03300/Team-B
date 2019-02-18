94b269006dc74a24c0746b37a9a87d41fa340458
EMILY <ley03300@gmail.com>
2019-02-18 11:51:27 -0800

    test

--- test.py
+++ test.py
@@ -66,9 +66,11 @@ if __name__ == '__main__':
             raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
             scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
             v = raw * scale
-            temp = (v - 500) / 10 + 45
-            print(temp)
-            sleep(1)
+            v20 = 345
+            temp = v - v20 + 20
+            # temp = (v - 500) / 10 + 45
+            # print(temp)
+            # sleep(1)
 
 
             # neo = Gpio()
