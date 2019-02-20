1522d8434384edda79457493aca193d03945e8a5
EMILY <ley03300@gmail.com>
2019-02-20 09:35:16 -0800

    test

--- test.py
+++ test.py
@@ -798,40 +798,8 @@
 #
 
 
-from btserver import BTServer
-from bterror import BTError
-
-import argparse
-import asyncore
-import json
-import datetime
-from random import uniform
-from threading import Thread
-from time import sleep, time
-from neo import Gpio
-
-
 from neo import Gpio  # import Gpio library
 from time import sleep  # import sleep to wait for blinks
-
-
-if __name__ == '__main__':
-# Create option parser
-usage = "usage: %prog [options] arg"
-parser = argparse.ArgumentParser()
-parser.add_argument("--output", dest="output_format", default="csv", help="set output format: csv, json")
-
-args = parser.parse_args()
-
-# Create a BT server
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-service_name = "GossipBTServer"
-server = BTServer(uuid, service_name)
-
-# Create the server thread and run it
-server_thread = Thread(target=asyncore.loop, name="Gossip BT Server Thread")
-server_thread.daemon = True
-server_thread.start()
 
 neo =Gpio()
 
@@ -976,25 +944,4 @@ while True:
     print("PM25: {} ".format(PM25))
     print("\n")
 
-    msg = ""
-    if args.output_format == "csv":
-        msg = "realtime, {}, {}, {}, {}, {}, {}, {}".format(epoch_time, temp, SN1, SN2, SN3, SN4, PM25)
-    # elif args.output_format == "json":
-    #     output = {'type': 'realtime',
-    #               'time': epoch_time,
-    #               'temp': temp,
-    #               'NO2_SN1': SN1,
-    #               'O3_SN2': SN2,
-    #               'CO_SN3': SN3,
-    #               'SO2_SN4': SN4,
-    #               'PM2.5': PM25}
-    #     msg = json.dumps(output)
-    try:
-        client_handler.send((msg + '\n').encode('ascii'))
-    except Exception as e:
-        BTError.print_error(handler=client_handler, error=BTError.ERR_WRITE, error_message=repr(e))
-        client_handler.handle_close()
-
-    # Sleep for 3 seconds
-    sleep(5)
 
