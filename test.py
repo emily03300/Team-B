a7db4b23cd0542ee267b5feee95adbc308836d46
EMILY <ley03300@gmail.com>
2019-02-20 00:23:53 -0800

    test

--- test.py
+++ test.py
@@ -879,7 +879,6 @@ while True:
 
     SN2 = ((c4 - 417) - 0.5 * (c5 - 402)) / 0.393
     print(SN2)
-    print(' ')
 
     # Alphasense SN3
     neo.digitalWrite(pinNum[0], 0)
@@ -903,6 +902,7 @@ while True:
     c7 = raw * scale
 
     SN3 = ((c6 - 265) - 0.44 * (c7 - 281)) / 0.292
+    print(SN3)
 
     # Alphasense SN4
     neo.digitalWrite(pinNum[0], 0)
@@ -926,6 +926,7 @@ while True:
     c9 = raw * scale
 
     SN4 = ((c8 - 275) - 0.6 * (c9 - 295))
+    print(SN4)
 
     # PM2.5
     neo.digitalWrite(pinNum[0], 1)
@@ -941,3 +942,4 @@ while True:
     hppcf = (240.0 * pow(c11, 6) - 2491.3 * pow(c11, 5) + 9448.7 * pow(c11, 4) - 14840.0 * pow(c11, 3) + 10684.0 * pow(
         c11, 2) + 2211.8 * (c11) + 7.9623)
     PM25 = 0.518 + .00274 * hppcf
+    print(PM25)
