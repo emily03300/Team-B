4cd5fc511579f7619927f62a132d59c02acf0885
EMILY <ley03300@gmail.com>
2019-02-20 01:01:55 -0800

    test

--- test.py
+++ test.py
@@ -837,23 +837,27 @@ if __name__ == '__main__':
 
 
 
-neo =Gpio()
 
-S0 = 24 # pin to use
-S1 = 25
-S2 = 26
-S3 = 27
-
-pinNum = [S0, S1, S2, S3]
-
-num = [0,0,0,0]
-
-# Blink example
-for i in range(4):
-    neo.pinMode(pinNum[i], neo.OUTPUT)
 
 
 while True:
+
+    neo = Gpio()
+
+    S0 = 24  # pin to use
+    S1 = 25
+    S2 = 26
+    S3 = 27
+
+    pinNum = [S0, S1, S2, S3]
+
+    num = [0, 0, 0, 0]
+
+    # Blink example
+    for i in range(4):
+        neo.pinMode(pinNum[i], neo.OUTPUT)
+
+
     neo.digitalWrite(pinNum[0], 0)
     neo.digitalWrite(pinNum[1], 0)
     neo.digitalWrite(pinNum[2], 0)
