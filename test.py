cb7d1a2e760d89bd46fa774bbbbf37617d670168
EMILY <ley03300@gmail.com>
2019-02-20 01:58:27 -0800

    test

--- test.py
+++ test.py
@@ -827,10 +827,10 @@ while True:
     raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
     scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
     v = raw * scale
-    t = (v - 590)/10
+    temp = (v - 590)/10
 
-    temp = (t * 1.8) + 32
-    print(temp)
+    temp = (temp * 1.8) + 32
+    print("temp: {} ".format(temp))
 
     # Alphasense SN1
     neo.digitalWrite(pinNum[0], 0)
@@ -854,7 +854,7 @@ while True:
     c3 = raw * scale
 
     SN1 = ((c2 - 286) - 0.75 * (c3 - 292)) / 0.258
-    print(SN1)
+    print("NO2: {} ".format(SN1))
 
     # Alphasense SN2
     neo.digitalWrite(pinNum[0], 0)
@@ -878,7 +878,7 @@ while True:
     c5 = raw * scale
 
     SN2 = ((c4 - 417) - 0.5 * (c5 - 402)) / 0.393
-    print(SN2)
+    print("O3: {} ".format(SN2))
 
     # Alphasense SN3
     neo.digitalWrite(pinNum[0], 0)
@@ -902,7 +902,7 @@ while True:
     c7 = raw * scale
 
     SN3 = ((c6 - 265) - 0.44 * (c7 - 281)) / 0.292
-    print(SN3)
+    print("CO: {} ".format(SN3))
 
     # Alphasense SN4
     neo.digitalWrite(pinNum[0], 0)
@@ -925,8 +925,8 @@ while True:
     scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
     c9 = raw * scale
 
-    SN4 = ((c8 - 275) - 0.6 * (c9 - 295))
-    print(SN4)
+    SN4 = ((c8 - 275) - 0.6 * (c9 - 295)) /0.3
+    print("SO2: {} ".format(SN4))
 
     # PM2.5
     neo.digitalWrite(pinNum[0], 1)
@@ -940,9 +940,9 @@ while True:
     c11 = (raw * scale) / 1000
 
     hppcf = (240.0 * pow(c11, 6) - 2491.3 * pow(c11, 5) + 9448.7 * pow(c11, 4) - 14840.0 * pow(c11, 3) + 10684.0 * pow(
-        c11, 2) + 2211.8 * (c11) + 7.9623)
+        c11, 2) + 2211.8 * c11 + 7.9623)
     PM25 = 0.518 + .00274 * hppcf
-    print(PM25)
+    print("PM25: {} ".format(PM25))
     print("\n")
 
 
