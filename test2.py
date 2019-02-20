6db6a54eef5e650d72ab458fbb4ee6bb172ad1b0
EMILY <ley03300@gmail.com>
2019-02-19 21:59:22 -0800

    test

--- test2.py
+++ test2.py
@@ -1,4 +1,5 @@
-raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
-v = raw * scale
-print(v)
+while True:
+    raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
+    scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
+    v = raw * scale
+    print(v)
