a636ae155bb8f6039473247ac431370a9a35ca65
EMILY <ley03300@gmail.com>
2019-02-19 20:10:39 -0800

    test

--- test.py
+++ test.py
@@ -670,7 +670,7 @@ if __name__ == '__main__':
             hppcf = (240.0 * pow(c11, 6) - 2491.3 * pow(c11, 5) + 9448.7 * pow(c11, 4) - 14840.0 * pow(c11, 3) + 10684.0 * pow(c11, 2) + 2211.8 * (c11) + 7.9623)
             PM25 = 0.518 + .00274 * hppcf
 
-
+            print('{} {} {} {} {} {}'.format(temp, SN1, SN2, SN3, SN4,PM25))
 
             msg = ""
             if args.output_format == "csv":
