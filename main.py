d9bb5bee9f525176aa6dcdb632e8dd2a0d63380d
EMILY <ley03300@gmail.com>
2019-02-23 17:09:36 -0800

    test

--- main.py
+++
@@ -1,4 +0,0 @@
-import datetime
-name=input("What's your name?")
-today=str(datetime.datetime.today().strftime('%Y-%m-%d'))
-print("Hello,"+name+"! today is "+today)
--- test2.py
+++
@@ -1,5 +0,0 @@
-import random as random
-
-while(True):
-    PM25 = random.uniform(10.0, 12.0)
-    print(PM25)
